b575d0a437a4b37b531548b891a42dea21a85e38
safinahussain <safinahusain@hotmail.com>
2015-08-25 20:35:49 +0100

    returns venues now
    Changed method to POST to allow full url to go through - returns venues.

--- WebContent/js/app.js
+++ WebContent/js/app.js
@@ -4,7 +4,7 @@ function search(){
 	
 	var locationValue = document.getElementbyId("location").value;
 	$.ajax({
-		  url: "https://api.foursquare.com/v2/venues/search?client_id=ANNWWLGLZ1TZYWX1KZQTZSKIYDFW2ZS30LOL20CBA4JM0RBP&client_secret=MSL014OPY1RLI4P2K35ZYMOFPO3GPKBLPFOF44KJFG2UBTGE&v=20130815&ll=London",
+		  url: "https://api.foursquare.com/v2/venues/search?ll=51.52,00.03&client_id=ANNWWLGLZ1TZYWX1KZQTZSKIYDFW2ZS30LOL20CBA4JM0RBP&client_secret=MSL014OPY1RLI4P2K35ZYMOFPO3GPKBLPFOF44KJFG2UBTGE&v=20150825",
 		  context: document.body
 		}).done(function() {
 		  alert("done");
